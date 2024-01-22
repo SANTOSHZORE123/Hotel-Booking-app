7a1626633dfb1781788b48f242fc82eb4a04d5fe
santosh janardan zore <47santoshzore@gmail.com>
2024-01-22 15:15:44 +0530

    build update

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
     try {
-      const res = await axios.post("http://localhost:8800/auth/login", credentials);
+      const res = await axios.post("/auth/login", credentials);
       console.log(res.data)
       dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
       navigate("/")
